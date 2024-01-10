82bf6259a7dd82e93affcc766025345bea06c309
_ <@>
2024-01-10 14:49:10 +0100

    Zmiany

--- app1-master-build/src/main.cpp
+++ app1-master-build/src/main.cpp
@@ -44,6 +44,38 @@ void create_task(Network* network, ArduinoNetworkAddress server_address, int32_t
 }
 
 
+void send_query_message(Network* network, ArduinoNetworkAddress server_address, char const* query) {
+    Tuple tuple = {
+	.element_count = 2,
+	.elements = (TupleElement*)malloc(2 * sizeof(TupleElement))
+    };
+
+    tuple.elements[0].type = tuple_string;
+    tuple.elements[0].data.data_string = alloc_string(query);
+
+    tuple.elements[1].type = tuple_int_template;
+
+    Message message = {
+        .id = message_next_id(),
+        .type = message_tuple_space_get_request,
+        .data = {
+            .tuple_space_get_request = {
+                .tuple_template = tuple,
+                .blocking_mode = tuple_space_nonblocking,
+                .remove_policy = tuple_space_remove,
+            },
+        },
+    };
+
+    OutboundMessage outbound_message = {
+        .message = message,
+        .receiver_address = server_address,
+    };
+
+    network_send_and_free_message(network, outbound_message);
+}
+
+
 void setup() {
     Network network = network_new(0);
 
@@ -57,6 +89,52 @@ void setup() {
         create_task(&network, server_address, i);
     }
 
+    bool* result_collected = (bool*)malloc(task_count * sizeof(bool));
+    unsigned collected_result_count = 0;
+
+    while (collected_result_count != task_count) {
+        for (size_t i = 0; i < task_count; ++i) {
+            if (!result_collected[i]) {
+                send_query_message(&network, server_address, "prime");
+
+                InboundMessage inbound_message = network_receive_message_blocking(&network);
+
+                switch (inbound_message.message.data.tuple_space_get_reply.result.status) {
+                    case tuple_space_success: {
+                        int32_t number = tuple_get_int(&inbound_message.message.data.tuple_space_get_reply.result.tuple, 1);
+                        result_collected[number] = true;
+                        collected_result_count += 1;
+                        Serial.print(number);
+                        Serial.println(F(" is prime"));
+                        break;
+                    }
+                    case tuple_space_failure: {
+                        send_query_message(&network, server_address, "not prime");
+
+                        InboundMessage inbound_message = network_receive_message_blocking(&network);
+
+                        switch (inbound_message.message.data.tuple_space_get_reply.result.status) {
+                            case tuple_space_success: {
+                                int32_t number = tuple_get_int(&inbound_message.message.data.tuple_space_get_reply.result.tuple, 1);
+                                result_collected[number] = true;
+                                collected_result_count += 1;
+                                Serial.print(number);
+                                Serial.println(F(" is not prime"));
+                                break;
+                            }
+                            case tuple_space_failure: {
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    free(result_collected);
 
     network_free(network);
 
